25dfb222af8134e0e8758258c26cb4f7c097c31f
Maxim Gorelikov <gorelikov.max@gmail.com>
2017-10-29 14:01:35 +0300

    Reactor streams with schedulers

--- hub/src/main/java/org/some/thing/card/hub/controller/HubController.java
+++ hub/src/main/java/org/some/thing/card/hub/controller/HubController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -23,18 +23,18 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class HubController {
   private final CardsService cardsService;
 
-  @RequestMapping(method = GET, value = "/cards", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Mono<List<Card>> loadCards(@RequestHeader("userId") String userId,
+  @RequestMapping(method = GET, value = "/cards", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<List<Card>> loadCards(@RequestHeader("userId") String userId,
                                     @RequestHeader("longitude") BigDecimal longitude,
                                     @RequestHeader("latitude") BigDecimal latitude,
                                     @RequestParam("currentDate") Long currentDate) {
-    return Mono.fromCallable(() ->cardsService.loadCards(UserData.builder()
+    return cardsService.loadCards(UserData.builder()
         .currentDate(currentDate)
         .userId(userId)
         .geoPosition(GeoPosition.builder()
             .latitude(latitude)
             .longitude(longitude)
             .build())
-        .build()));
+        .build());
   }
 }
--- hub/src/main/java/org/some/thing/card/hub/service/RemoteCardsHubService.java
+++ hub/src/main/java/org/some/thing/card/hub/service/RemoteCardsHubService.java
@@ -6,7 +6,10 @@ import org.some.thing.card.entity.Card;
 import org.some.thing.card.entity.UserData;
 import org.some.thing.card.hub.client.CardClient;
 import org.springframework.stereotype.Service;
-import java.util.Collections;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,21 +20,19 @@ public class RemoteCardsHubService implements CardsService {
   private final List<CardClient> cardClients;
 
   @Override
-  public List<Card> loadCards(UserData userData) {
-    return cardClients.stream()
-        .parallel()
-        .flatMap(client -> getCards(userData, client).stream())
-        .collect(Collectors.toList());
+  public Flux<List<Card>> loadCards(UserData userData) {
+    final List<Mono<List<Card>>> monoList = cardClients.stream()
+            .map(client -> getCards(userData, client))
+            .collect(Collectors.toList());
+    return Flux.merge(monoList);
   }
 
   @SuppressWarnings("unchecked")
-  private List<Card> getCards(UserData userData, CardClient client) {
-    try {
-      return client.getCards(userData.getUserId(),
-          userData.getGeoPosition().getLongitude(), userData.getGeoPosition().getLatitude(),
-          userData.getCurrentDate());
-    } catch (Exception e) {
-      return Collections.emptyList();
-    }
+  private Mono<List<Card>> getCards(UserData userData, CardClient client) {
+      return Mono.<List<Card>>fromCallable(() ->
+              client.getCards(userData.getUserId(),
+                      userData.getGeoPosition().getLongitude(), userData.getGeoPosition().getLatitude(),
+                      userData.getCurrentDate()))
+              .subscribeOn(Schedulers.elastic());
   }
 }
