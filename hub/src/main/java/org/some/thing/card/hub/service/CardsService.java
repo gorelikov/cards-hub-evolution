12143c59fb89dbb230e36b86bf938631a7b045eb
Maxim Gorelikov <gorelikov.max@gmail.com>
2017-11-03 00:25:35 +0300

    Reactor streams with schedulers

--- hub/src/main/java/org/some/thing/card/hub/service/CardsService.java
+++ hub/src/main/java/org/some/thing/card/hub/service/CardsService.java
@@ -2,9 +2,10 @@ package org.some.thing.card.hub.service;
 
 import org.some.thing.card.entity.Card;
 import org.some.thing.card.entity.UserData;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
 public interface CardsService {
-    List<Card> loadCards(UserData userData);
+    Flux<List<Card>> loadCards(UserData userData);
 }
